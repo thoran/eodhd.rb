83319a0276d1d390d5463bff04d34137cd0a2062
thoran <code@thoran.com>
2024-10-10 02:29:08 +1100

    Committed automatically by git-importall.

--- lib/Eodhistoricaldata.rb
+++ lib/Eodhistoricaldata.rb
@@ -1,8 +1,8 @@
 # Eodhistoricaldata.rb
 # Eodhistoricaldata
 
-# 20230412
-# 0.1.1
+# 20230419
+# 0.2.0
 
 gem 'http.rb'
 require 'http.rb'
@@ -10,53 +10,71 @@ require 'Pd/PasswordFile'
 require 'Pd/Password'
 require 'json'
 
-def init
-  if File.exist?(Pd::PasswordFile.encrypted_filename)
-    Pd::Password.from_csv(Pd::PasswordFile.read)
+class ApiToken
+  class << self
+    def init
+      if File.exist?(Pd::PasswordFile.encrypted_filename)
+        Pd::Password.from_csv(Pd::PasswordFile.read)
+      end
+    end
+
+    def label
+      'eodhistoricaldata.com-api_token'
+    end
+
+    def password
+      @password ||= (
+        init
+        Pd::Password.find(label).first
+      )
+    end
+
+    def api_token
+      @api_token ||= password.to_h[:password]
+    end
   end
 end
 
-def label
-  'eodhistoricaldata.com-api_token'
-end
+class Eodhistoricaldata
+  def initialize
+    @api_token = ApiToken.api_token
+  end
 
-def password
-  @password ||= Pd::Password.find(label).first
-end
+  # This endpoint always returns json regardless of what fmt is specified.
+  def exchanges_list
+    request_string = "https://eodhistoricaldata.com/api/exchanges-list"
+    do_request(request_string: request_string)
+  end
 
-def api_token
-  @api_token ||= password.to_h[:password]
-end
+  def exchange_symbol_list(exchange_code:)
+    request_string = "https://eodhistoricaldata.com/api/exchange-symbol-list/#{exchange_code}"
+    do_request(request_string: request_string)
+  end
 
-def exchanges_list
-  request_string = "https://eodhistoricaldata.com/api/exchanges-list"
-  args = {api_token: api_token} # fmt is always json
-  response = HTTP.get(request_string, args)
-  JSON.parse(response.body)
-end
+  def eod_data(exchange_id:, symbol:, period: 'd')
+    request_string = "https://eodhistoricaldata.com/api/eod/#{symbol}.#{exchange_id}"
+    args = {period: period}
+    do_request(request_string: request_string, args: args)
+  end
 
-def exchange_symbol_list(exchange_code:)
-  request_string = "https://eodhistoricaldata.com/api/exchange-symbol-list/#{exchange_code}"
-  args = {api_token: api_token, fmt: 'json'}
-  response = HTTP.get(request_string, args)
-  JSON.parse(response.body)
-end
+  private
 
-def eod_data(exchange_id:, symbol:, period: 'd')
-  request_string = "https://eodhistoricaldata.com/api/eod/#{symbol}.#{exchange_id}"
-  args = {api_token: api_token, fmt: 'json', period: period}
-  response = HTTP.get(request_string, args)
-  JSON.parse(response.body)
+  def do_request(request_string:, args: {})
+    api_token = args[:api_token] || @api_token
+    fmt = args[:fmt] || 'json'
+    args.merge!({api_token: api_token, fmt: fmt})
+    response = HTTP.get(request_string, args)
+    JSON.parse(response.body)
+  end
 end
 
 def main
-  init
-  exchanges_list.each do |exchange|
-    p exchange
-    exchange_symbol_list(exchange_code: exchange["Code"]).each do |symbol|
-      puts "#{exchange["Code"]}:#{symbol["Code"]}"
-      p eod_data(exchange_id: exchange["Code"], symbol: symbol["Code"])
-    end
+  exchange_code = 'AU'
+  eod_client = Eodhistoricaldata.new
+  symbols = eod_client.exchange_symbol_list(exchange_code: exchange_code)
+  symbols.each do |symbol|
+    puts "#{exchange_code}:#{symbol["Code"]}"
+    p eod_client.eod_data(exchange_id: exchange_code, symbol: symbol["Code"])
   end
 end
 
