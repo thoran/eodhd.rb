ef0e4994d485a448417230abc3052b9834b58ff0
thoran <code@thoran.com>
2024-10-10 02:29:19 +1100

    Committed automatically by git-importall.

--- lib/eod_historical_data.rb
+++ lib/eod_historical_data.rb
@@ -1,18 +1,20 @@
 # eod_historical_data.rb
 
-# 20240602
-# 0.11.2
+# 20240706
+# 0.11.3
 
 # Changes since 0.10:
 # -/0 (+ bulk data API)
 # 1. + EodHistoricalData#eod_bulk_last_day
 # 2. + EodHistoricalData::EodBulkLastDay
-# 0/1 (+ tests, gemification-incomplete)
+# 0/1 (+ tests, gemification—incomplete)
 # 3. ~ EodHistoricalData::EodData#load: Use the row values.
 # 1/2 (/each/collect/)
 # 4. ~ EodHistoricalData::BulkLastDay.load: /each/collect/
 # 5. ~ EodHistoricalData::EodData.load: /each/collect/
 # 6. ~ EodHistoricalData::Exchange.load: /each/collect/
 # 7. ~ EodHistoricalData::ExchangeSymbol.load: /each/collect/
+# 2/3
+# 8. ~ EodHistoricalData::EodData#load: Use the row values again after regression.
 
 require_relative './EodHistoricalData'
